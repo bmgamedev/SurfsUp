5fb47794a9bdc8973ff717d08688de774f2eb6e7
bmgamedev <r.c.mcgowan@hotmail.co.uk>
2017-01-22 01:22:24 +0000

    Beta v3
    Finished game. Futher updates will be non-essential features. Base game
    now complete.

--- Assets/Scripts/PC.cs
+++ Assets/Scripts/PC.cs
@@ -1,32 +1,63 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+using UnityEngine.UI;
+
 public class PC : MonoBehaviour {
 	
 	private Animator m_Animator;
 
 	private bool isJumping;
 	private bool isDucking;
-
+	
 	private float jumpHeight = 0.3f;
 	private float stdHeight = -0.6f;
+
+	private int levelSpeed;
+	private string levelName;
+
+	//private Transform m_GroundCheck;
+	//public LayerMask GroundLayer;
+
+	private int shellCount;
+	private int maxShells;
+	public Text countText;
+
 	
 	void Start () {
 		m_Animator = GetComponent<Animator>();
+
+		//m_GroundCheck = transform.FindChild("GroundCheck");
+
+		countText = GetComponent<Text>();
+		shellCount = 0;
+		maxShells = 13;
+		//SetCountText ();
+
+		levelName = Application.loadedLevelName;
+		int. TryParse(levelName, out levelSpeed);
+
 	}
 
 	void Update () {
 
 		//Time.timeScale = 0.2F;
-		float translation = Time.deltaTime * 2;
+		//float translation = Time.deltaTime * 3;
+		float translation = Time.deltaTime * levelSpeed;
 		transform.Translate(translation, 0, 0);
 
 	}
 
 	void OnTriggerEnter2D (Collider2D trigger) {
 		if (trigger.collider2D.tag == "Finish") {
-			print ("Winner");
-			Application.LoadLevel ("win");//load win screen;
+			//print ("Winner");
+			if (shellCount < maxShells){
+				Application.LoadLevel ("noshells");
+			} else if (Application.loadedLevelName != "5"){
+				Application.LoadLevel(Application.loadedLevel + 1);
+			} else {
+				Application.LoadLevel ("win");//load win screen;
+			}
 		} else if (trigger.collider2D.tag == "obstacle") {
 			print ("TRIGGERED!!!!"); //move player backwards;
 			this.transform.position = new Vector3 (transform.position.x-1.0f, this.transform.position.y, this.transform.position.z);
@@ -35,7 +66,11 @@ public class PC : MonoBehaviour {
 			//Invoke("SpeedUp",2);
 		} else if (trigger.collider2D.tag == "tsunami") {
 			print ("Game Over");
-			Application.LoadLevel ("lose");//load lose screen;
+			Application.LoadLevel ("wipeout");//load lose screen;
+		} else if (trigger.collider2D.tag == "shell"){
+			shellCount = shellCount + 1;
+			print (shellCount);
+			//SetCountText ();
 		}
 	} 
 
@@ -50,6 +85,23 @@ public class PC : MonoBehaviour {
 
 		m_Animator.SetBool("isJumping", isJumping);
 		m_Animator.SetBool("isDucking", isDucking);
+
+
+//		//****EXPERIMENTAL JUMP STUFF BELOW:*****
+//		bool isGrounded = Physics2D.OverlapPoint (m_GroundCheck.position, GroundLayer);
+//		if(Input.GetButton("Jump")){
+//			print ("jump pressed");
+//			Animator animator = GetComponent<Animator>() as Animator;
+//
+//			if(isGrounded){
+//				print ("jump successful");
+//				animator.SetTrigger("isJumping");
+//				rigidbody2D.AddForce(Vector2.up * 5f, ForceMode2D.Impulse);
+//			}
+//		}
+
+
+		//****WORKING JUMP CODE BELOW******
 
 		if(Input.GetButton("Jump")){
 			Animator animator = GetComponent<Animator>() as Animator;
@@ -68,6 +120,11 @@ public class PC : MonoBehaviour {
 		}
 
 	}
+
+	//void SetCountText()
+	//{
+	//	countText.text = "x " + shellCount.ToString ();
+	//}
 	
 //	void SlowDown()
 //	{
--- Assets/Scripts/Wave.cs
+++ Assets/Scripts/Wave.cs
@@ -4,14 +4,20 @@ using System.Collections;
 public class Wave : MonoBehaviour {
 
 	public Transform target;
+
+	private int levelSpeed;
+	private string levelName;
 	
 	void Start () {
 	
+		levelName = Application.loadedLevelName;
+		int. TryParse(levelName, out levelSpeed);
+
 	}
 
 	void Update () {
 
-		float translation = Time.deltaTime * 2;
+		float translation = Time.deltaTime * levelSpeed;
 		transform.Translate(translation, 0, 0);
 
 	}
